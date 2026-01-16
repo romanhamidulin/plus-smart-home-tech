bb2e5e4dd0ab1c2ad631f8db06254bd3fd61a113
Роман Хамидулин <romanhamidulin@ya.ru>
2026-01-16 13:30:38 +0500

    реализация Collector, правка

--- telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/controller/EventController.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/controller/EventController.java
@@ -1,10 +1,19 @@
 package ru.yandex.practicum.telemetry.collector.controller;
 
+import com.google.protobuf.Empty;
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
+import io.grpc.stub.StreamObserver;
 import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.server.service.GrpcService;
+import ru.yandex.practicum.grpc.telemetry.collector.CollectorControllerGrpc;
+import ru.yandex.practicum.grpc.telemetry.event.HubEventProto;
+import ru.yandex.practicum.grpc.telemetry.event.SensorEventProto;
 import ru.yandex.practicum.telemetry.collector.service.hub.HubEventHandler;
 import ru.yandex.practicum.telemetry.collector.service.sensor.SensorEventHandler;
 
 import java.util.Map;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
